07ba38ba23f1dd35a786a8febe0f904144cb3f6a
Abhilash Srivastava <abhilash.shakti@gmail.com>
2017-06-20 19:09:52 +0530

    Add the data split logic in py file

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -27,3 +27,5 @@ Y = labelencoder_Y.fit_transform(Y)
 
 # Splitting the dataset into Training set and Test set
 from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, 
+                                                    random_state = 0)
