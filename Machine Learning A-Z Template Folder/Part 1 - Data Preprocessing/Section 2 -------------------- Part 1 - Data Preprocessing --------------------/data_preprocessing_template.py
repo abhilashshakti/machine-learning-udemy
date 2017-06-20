d74aa122da09717d8f21392d8487aa848d34fb41
Abhilash Srivastava <abhilash.shakti@gmail.com>
2017-06-20 19:31:48 +0530

    Adding garbage comment

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -29,3 +29,4 @@ Y = labelencoder_Y.fit_transform(Y)
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, 
                                                     random_state = 0)
+# garbage comment
