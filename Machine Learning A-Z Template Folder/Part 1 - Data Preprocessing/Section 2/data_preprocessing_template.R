3cf8c30c8185c0bfb3e579c96029ff7b8b113d59
Abhilash Srivastava <abhilash.shakti@gmail.com>
2017-06-20 21:31:05 +0530

    Data Preprocessing final changes

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2/data_preprocessing_template.R
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2/data_preprocessing_template.R
@@ -2,24 +2,7 @@
 
 # Importing the dataset
 dataset = read.csv("Data.csv")
-
-# Fill in the missing values
-dataset$Age = ifelse(is.na(dataset$Age), 
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
-
-dataset$Salary = ifelse(is.na(dataset$Salary), 
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary)
-
-# Encoding categorical data
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-
-dataset$Purchased = factor(dataset$Purchased,
-                         levels = c('No', 'Yes'),
-                         labels = c(0, 1))
+# dataset = dataset[, 2:3]
 
 # Splitting the training set and test set
 install.packages('caTools')
@@ -30,8 +13,8 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[,2:3] = scale(training_set[,2:3])
-test_set[,2:3] = scale(test_set[,2:3])
+# training_set[,2:3] = scale(training_set[,2:3])
+# test_set[,2:3] = scale(test_set[,2:3])
 
 
 
